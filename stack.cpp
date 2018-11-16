2e1e6af792d5e62fc518078810edb5e79301406c
viralnatani <42761307+viralnatani@users.noreply.github.com>
2018-11-16 09:43:14 +0530

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int op,n=10,top=0,var=0,i=0;
+	int array[n];
+	while(1)
+	{
+		cout << 1. push 2. pop 3. display
+		switch(operation)
+		{
+			case 1:		
+				
+					for(int j=0; ;j++)
+					{
+						if(top==n-1)
+					{
+						cout << "overflow";
+						brearrayk;
+		 			} 
+					else if(top== -1)
+					{
+						 top = 0;
+					}
+					cout<<"element to push : ";
+					cin >> array[i];
+					i++;
+					top++;
+						}
+						
+						
+					}
+					break;
+			case 2:
+		
+		
+					
+					for(int k=0; k<top; k++)
+					{
+						if(top==-1)
+					{
+						cout << "starrayck is empty.";
+		 			} 
+					else
+					{
+						top=top-1;
+					}
+						cout << array[k] << endl;
+					}
+					break;
+		
+			case 3:
+					for(int k=0; k<top; k++)
+					{
+						cout << array[k] << endl;
+					}
+					exit(0);
+		}
+	}
+	
+	
+}
